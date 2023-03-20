4d409fc284e4ab8f79d7ebb318769b538d267b42
ZingaG <trad_bar@mail.ru>
2023-03-20 16:31:37 +0300

    Task 1 completed

--- Program.cs
+++ Program.cs
@@ -1,1 +1,37 @@
-﻿
+﻿int TakeDigit(string a)
+{
+    System.Console.WriteLine(a);
+    int result = Int32.Parse(System.Console.ReadLine());
+    return result;
+}
+
+int[,] CreateDoubleArray(int m, int n)
+{
+    int[,] array = new int[m,n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i,j] = Random.Shared.Next(0,10);
+        }
+    }
+
+    return array;
+}
+
+void PrintDoubleArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+           System.Console.Write(array[i,j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+
+int[,] RandomArray = CreateDoubleArray(TakeDigit("Input m = "), TakeDigit("Input n = "));
+
+PrintDoubleArray(RandomArray);
